f509800b598978224855f6215668a7035b5ed736
ThMenezes <thita.menezes24@gmail.com>
2023-01-17 13:04:15 -0300

    adicionando barra de progresso

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,23 @@ const inputSenha = document.querySelector('#senha')
 const inputSituacao = document.querySelector('#situacao')
 const inputMensagem = document.querySelector('#mensagem')
 
+const progresso = document.querySelector("#progresso");
+
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
     if(inputNome.value === "") {
         alert('Por favor, preencha seu nome.');
+        return;
+    }
+
+    if(inputCpf.value === "") {
+        alert('Por favor, insira os números do CPF.');
+        return;
+    }
+
+    if(inputNascimento.value === "") {
+        alert('Por favor, preencha sua data de nascimento.');
         return;
     }
 
@@ -31,6 +43,8 @@ formulario.addEventListener("submit", (evento) => {
     }
 
     formulario.submit()
+
+    progresso.value = 0;
 })
 
 // validação do email com regex
@@ -52,4 +66,19 @@ function validacaoSenha(senha, minDigitos) {
     return false
 }
 
+// Atualiza a barra de progresso ao preencher o formulário
+formulario.addEventListener("input", () => {
+    const todosInputs = formulario.elements.length - 1;
+    let camposCompletos = 0;
+  
+    // Conta o número de campos preenchidos
+    for (let i = 0; i < todosInputs; i++) {
+      if (formulario.elements[i].value) {
+        camposCompletos++;
+      }
+    }
+  
+    // Atualiza o valor da barra de progresso
+    progresso.value = (camposCompletos / todosInputs) * 100;
+  });
 
